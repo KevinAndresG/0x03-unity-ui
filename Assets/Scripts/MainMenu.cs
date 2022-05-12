b2ae60728009194728380d5f596b1fa32faad8b9
KevinAndresG <kevingar1414@gmail.com>
2022-05-12 00:32:42 -0500

    both options and back button working now

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void PlayMaze()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
     private void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            SceneManager.LoadScene("menu");
         }
+    }
+    public void PlayMaze()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
