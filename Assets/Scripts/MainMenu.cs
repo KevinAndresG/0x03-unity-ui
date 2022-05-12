7541fe51cfdd83cc447ed5177d007a27b865b8a9
KevinAndresG <kevingar1414@gmail.com>
2022-05-12 02:38:08 -0500

    blind mode implemented

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+    private bool stateOfCB;
     private void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
         {
+            
             SceneManager.LoadScene("menu");
         }
     }
     public void PlayMaze()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else if (colorblindMode.isOn == false)
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
     public void QuitMaze()
     {
